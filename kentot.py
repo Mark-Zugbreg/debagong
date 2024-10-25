c5b07dc423bd8288af3af50983833617f2a9ac7b
kroco <151419916+Mark-Zugbreg@users.noreply.github.com>
2024-10-25 15:05:50 +0700

    Add files via upload

---
+++ kentot.py
@@ -0,0 +1,10 @@
+from shutil import which
+from flask import Flask
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+	return f"ffmpeg <h1>{str(which('ffmpeg'))}</h1>"
+
+if __name__ == "__main__":
+	app.run(port=8000)
