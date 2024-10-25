b6b4e5558e2085450207296c76666722ee23c3bd
kroco <151419916+Mark-Zugbreg@users.noreply.github.com>
2024-10-25 16:10:38 +0700

    Update kentot.py

--- kentot.py
+++ kentot.py
@@ -4,6 +4,11 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
+	return "hallo kontol"
+
+
+@app.route("/ngecek", methods=["GET"]):
+def asyu():
 	return f"ffmpeg <h1>{str(which('ffmpeg'))}</h1>"
 
 if __name__ == "__main__":
